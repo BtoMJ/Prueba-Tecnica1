e7bdfd5247f3663d5d91427281363880cd332467
BtoMJ <danny_mj17@hotmail.com>
2022-01-03 10:12:29 -0600

    Actualizando carpeta cliente

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="FooterContainer">
+            Diseño desarrollado por Daniel Mena
+        </div>
+    )
+}
+
+export default Footer
